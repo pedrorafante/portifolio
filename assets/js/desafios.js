ab104fc6a9b5b81dcd6cda376e2550264f2a3938
Pedro Rafante <pedrorafante@gmail.com>
2023-04-21 21:11:47 -0300

    Ajustando detalhes finais do portifolio

---
+++ assets/js/desafios.js
@@ -0,0 +1,6 @@
+function desafios(section) {
+  section.innerHTML =
+    ``
+}
+
+export { desafios }
---
+++ assets/js/projetos.js
@@ -0,0 +1,43 @@
+function projetos(section) {
+  section.innerHTML = `<header>
+      <h2>Projetos</h2>
+    </header>
+    <div class="content">
+      <div class="cards">
+        <div></div>
+        <div class="card">
+          <h3>Em breve...</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path
+              d="M5 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm14 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-7 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+              fill="rgba(255,255,255,1)"
+            />
+          </svg>
+          <p>
+            Estarei atualizando os projetos. <br />
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path fill="none" d="M0 0H24V24H0z" />
+            <path
+              d="M21 3v2c0 9.627-5.373 14-12 14H5.243C5.08 19.912 5 20.907 5 22H3c0-1.363.116-2.6.346-3.732C3.116 16.974 3 15.218 3 13 3 7.477 7.477 3 13 3c2 0 4 1 8 0zm-8 2c-4.418 0-8 3.582-8 8 0 .362.003.711.01 1.046 1.254-1.978 3.091-3.541 5.494-4.914l.992 1.736C8.641 12.5 6.747 14.354 5.776 17H9c6.015 0 9.871-3.973 9.997-11.612-1.372.133-2.647.048-4.22-.188C13.627 5.027 13.401 5 13 5z"
+              fill="rgba(255,255,255,1)"
+            />
+          </svg>
+        </div>
+        <div></div>
+      </div>
+    </div>`;
+}
+
+export { projetos };
